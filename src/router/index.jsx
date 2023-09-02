f5e7abf2166ef682c22c1ac04c049b27d6f62af8
Davranbek <davranbek_1995@mail.ru>
2023-09-02 10:14:07 +0500

    changed root directory

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,15 +4,16 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter } from "react-router-dom";
 
-import Root from "../Root";
+import Layout from "../pages/Layout/Layout";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
 
+
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Root/>,
+        element: <Layout/>,
         errorElement: <NotFound />,
         children: [
             {
